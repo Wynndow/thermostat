3c331cac585ac2e7a5734ba309c7f653e45f3caf
Chris Wynne <chris.wynndow@gmail.com>
2016-01-06 16:41:33 +0000

    Links buttons to jQuery

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $("#temperature").html(thermostat.getCurrentTemperature());
-});
 
+  $('#temp_up').click(function() {
+    thermostat.increaseTemperature();
+    $("#temperature").html(thermostat.getCurrentTemperature());
+  });
+
+  $('#temp_down').click(function() {
+    thermostat.decreaseTemperature();
+    $("#temperature").html(thermostat.getCurrentTemperature());
+  });
+
+  $('#temp_reset').click(function() {
+    thermostat.reset();
+    $("#temperature").html(thermostat.getCurrentTemperature());
+  });
+
+  $('#powersave').change(function() {
+    thermostat.togglePowerSaving();
+    $("#temperature").html(thermostat.getCurrentTemperature());
+  });
+
+});
