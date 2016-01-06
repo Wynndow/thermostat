61f8596ad2eb68209616b7de9fc822c4278aa5ad
Chris Wynne <chris.wynndow@gmail.com>
2016-01-06 16:44:57 +0000

    Changes background color with temp

--- src/interface.js
+++ src/interface.js
@@ -2,24 +2,36 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   $("#temperature").html(thermostat.getCurrentTemperature());
 
+  $('body').css('background-color', thermostat.getColour());
+
+
   $('#temp_up').click(function() {
     thermostat.increaseTemperature();
     $("#temperature").html(thermostat.getCurrentTemperature());
+    $('body').css('background-color', thermostat.getColour());
+
   });
 
   $('#temp_down').click(function() {
     thermostat.decreaseTemperature();
     $("#temperature").html(thermostat.getCurrentTemperature());
+    $('body').css('background-color', thermostat.getColour());
+
   });
 
   $('#temp_reset').click(function() {
     thermostat.reset();
     $("#temperature").html(thermostat.getCurrentTemperature());
+    $('body').css('background-color', thermostat.getColour());
+
   });
 
   $('#powersave').change(function() {
     thermostat.togglePowerSaving();
     $("#temperature").html(thermostat.getCurrentTemperature());
+    $('body').css('background-color', thermostat.getColour());
+
   });
+
 
 });
